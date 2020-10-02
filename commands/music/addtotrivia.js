3b9fd1c78038984562fbdcf40f5533d0c3091d8b
Mika <makomitsuki@gmail.com>
2020-10-01 21:23:30 -0500

    check query

--- commands/music/addtotrivia.js
+++ commands/music/addtotrivia.js
@@ -12,12 +12,26 @@ module.exports = class AddToTriviaCommand extends Command {
       aliases: ['add-to-trivia', 'add-song-to-trivia'],
       memberName: 'addtotrivia',
       group: 'music',
-      description: "Contribute a song to the Music Trivia library!"
+      description: "Contribute a song to the Music Trivia library!",
+      throttling: {
+        usages: 2,
+        duration: 5
+      },
+      args: [
+        {
+          key: 'link',
+          prompt: 'What is the link to the song you want to add?',
+          type: 'string',
+          validate: function(query) {
+            return query.length > 0 && query.length < 200;
+          }
+        }
+      ]
     });
   }
 
   async run(message) {
-    /*// check if trivia is running
+    // check if trivia is running
     if (message.guild.triviaData.isTriviaRunning == true) {
       message.say('Please try after the trivia has ended');
       return;
@@ -28,8 +42,23 @@ module.exports = class AddToTriviaCommand extends Command {
       'resources/music/musictrivia.json',
       'utf8'
     );
-    var videoDataArray = JSON.parse(jsonSongs).songs;*/ return;
+    var videoDataArray = JSON.parse(jsonSongs).songs;
 
+    // check youtube url
+    if (query.match(/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+/)) {
+      query = query
+        .replace(/(>|<)/gi, '')
+        .split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+    }
+    const id = query[2].split(/[^0-9a-z_\-]/i)[0];
+      const video = await youtube.getVideoByID(id).catch(function() {
+        message.say('There was a problem getting the video you provided!');
+        return;
+      });
 
-  }
+      if (video.raw.snippet.liveBroadcastContent === 'live') {
+        return message.say("Live streams are not allowed!");
+      }
+    }
+
 };
